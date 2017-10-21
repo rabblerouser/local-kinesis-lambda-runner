495bfae1239b2ba83b72921e49e190b980777b0b
Cam Jackson <camjackson89@gmail.com>
2017-10-21 20:17:44 +0800

    Extract deps so they can be injected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+const AWS = require('aws-sdk')
+
 const pollKinesis = require('./pollKinesis');
 
+const streamName = process.env.STREAM_NAME;
+const kinesis = new AWS.Kinesis({
+  endpoint: process.env.KINESIS_ENDPOINT,
+  region: 'ap-southeast-2',
+  accessKeyId: 'FAKE',
+  secretAccessKey: 'ALSO FAKE',
+});
+
 module.exports = {
-  pollKinesis,
+  pollKinesis(kinesis, streamName),
 };
--- src/pollKinesis.js
+++ src/pollKinesis.js
@@ -1,63 +1,55 @@
-const AWS = require('aws-sdk')
+module.exports = (kinesis, StreamName) => {
+  const wait = ms => () => new Promise(resolve => setTimeout(resolve, ms));
 
-const StreamName = process.env.STREAM_NAME;
-const kinesis = new AWS.Kinesis({
-  endpoint: process.env.KINESIS_ENDPOINT,
-  region: 'ap-southeast-2',
-  accessKeyId: 'FAKE',
-  secretAccessKey: 'ALSO FAKE',
-});
+  const callback = (err, result) => err ? console.error('Handler failed:', err.message) : console.log('Handler suceeded:', result);
 
-const wait = ms => () => new Promise(resolve => setTimeout(resolve, ms));
+  const mapKinesisRecord = record => ({
+    data: record.Data.toString('base64'),
+    sequenceNumber: record.SequenceNumber,
+    approximateArrivalTimestamp: record.ApproximateArrivalTimestamp,
+    partitionKey: record.PartitionKey,
+  });
 
-const callback = (err, result) => err ? console.error('Handler failed:', err.message) : console.log('Handler suceeded:', result);
+  const reduceRecord = lambda => (promise, kinesisRecord) => promise.then(() => {
+    const singleRecordEvent = { Records: [{ kinesis: mapKinesisRecord(kinesisRecord) }] };
+    console.log('Invoking lambda with record from stream:', JSON.stringify(singleRecordEvent));
+    return lambda(singleRecordEvent, null, callback);
+  });
 
-const mapKinesisRecord = record => ({
-  data: record.Data.toString('base64'),
-  sequenceNumber: record.SequenceNumber,
-  approximateArrivalTimestamp: record.ApproximateArrivalTimestamp,
-  partitionKey: record.PartitionKey,
-});
+  const pollKinesis = lambda => firstShardIterator => {
+    const fetchAndProcessRecords = shardIterator => (
+      kinesis.getRecords({ ShardIterator: shardIterator }).promise().then(records => (
+        records.Records.reduce(reduceRecord(lambda), Promise.resolve())
+          .then(wait(500))
+          .then(() => fetchAndProcessRecords(records.NextShardIterator))
+      ))
+    )
+    return fetchAndProcessRecords(firstShardIterator);
+  }
 
-const reduceRecord = lambda => (promise, kinesisRecord) => promise.then(() => {
-  const singleRecordEvent = { Records: [{ kinesis: mapKinesisRecord(kinesisRecord) }] };
-  console.log('Invoking lambda with record from stream:', JSON.stringify(singleRecordEvent));
-  return lambda(singleRecordEvent, null, callback);
-});
+  const run = lambda => {
+    const loop = () => {
+      return kinesis.describeStream({ StreamName }).promise()
+        .then(stream => {
+          console.log(`Found ${StreamName}!`);
+          const ShardId = stream.StreamDescription.Shards[0].ShardId
 
-const pollKinesis = lambda => firstShardIterator => {
-  const fetchAndProcessRecords = shardIterator => (
-    kinesis.getRecords({ ShardIterator: shardIterator }).promise().then(records => (
-      records.Records.reduce(reduceRecord(lambda), Promise.resolve())
-        .then(wait(500))
-        .then(() => fetchAndProcessRecords(records.NextShardIterator))
-    ))
-  )
-  return fetchAndProcessRecords(firstShardIterator);
-}
-
-const run = lambda => {
-  const loop = () => {
-    return kinesis.describeStream({ StreamName }).promise()
-      .then(stream => {
-        console.log(`Found ${StreamName}!`);
-        const ShardId = stream.StreamDescription.Shards[0].ShardId
-
-        const params = { StreamName, ShardId, ShardIteratorType: 'LATEST' };
-        return kinesis.getShardIterator(params).promise();
-      })
-      .then(shardIterator => {
-        console.log('Polling kinesis for events...');
-        return shardIterator.ShardIterator;
-      })
-      .then(pollKinesis(lambda))
-      .catch(err => {
-        console.error(err.message);
-        console.log('Restarting...');
-        setTimeout(loop, 2000);
-      });
+          const params = { StreamName, ShardId, ShardIteratorType: 'LATEST' };
+          return kinesis.getShardIterator(params).promise();
+        })
+        .then(shardIterator => {
+          console.log('Polling kinesis for events...');
+          return shardIterator.ShardIterator;
+        })
+        .then(pollKinesis(lambda))
+        .catch(err => {
+          console.error(err.message);
+          console.log('Restarting...');
+          setTimeout(loop, 2000);
+        });
+    };
+    loop();
   };
-  loop();
-};
 
-module.exports = run;
+  return run;
+};
